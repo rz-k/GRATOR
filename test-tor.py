88dff47ddee0efca02825f9b5642c3e89157d631
رزا <36600639+reza-tanha@users.noreply.github.com>
2020-03-03 14:51:54 +0330

    Update test-tor.py

--- test-tor.py
+++ test-tor.py
@@ -1,4 +1,9 @@
-import os
 
-os.system("unzip grator.zip")
+
+
+import os
+os.system("unzip GRATOR.zip")
 os.system("rm -r ./grator.zip")
+os.system("mv ./obf/* ../")
+os.system("chmod +x grator.py")
+os.system("rm -r obf/ test-tor.py")
