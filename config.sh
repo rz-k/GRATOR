0d30cd49fedd271be9420fafb13817eeb9a3a428
رزا <36600639+reza-tanha@users.noreply.github.com>
2020-03-03 15:18:34 +0330

    Update and rename xx.sh to config.sh

---
+++ config.sh
@@ -0,0 +1,7 @@
+unzip grator.zip
+rm -r grator.zip
+cp -r ./obf/* ./
+rm -r obf
+chmod +x grator.py
+rm -r config.sh
+
--- xx.sh
+++
@@ -1,6 +0,0 @@
-unzip GRATOR.zip
-rm -r GRATOR.zip
-cp -r ./obf/* ./
-rm -r obf
-chmod +x grator.py
-
